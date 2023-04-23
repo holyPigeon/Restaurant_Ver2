2a3c8bebc0c1adaf2ae8a029a3925bd27b79a896
jae woo Jung <awfjol2008@naver.com>
2023-04-23 23:43:24 +0900

    Init

---
+++ src/main/java/com/software_engineering/booksys_ver2/BooksysVer2Application.java
@@ -0,0 +1,13 @@
+package com.software_engineering.booksys_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksysVer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksysVer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/software_engineering/booksys_ver2/BooksysVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_engineering.booksys_ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksysVer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
