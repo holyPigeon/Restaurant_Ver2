a7e5a3268d182e7d0b2d29c528b88802fd13602e
jae woo Jung <awfjol2008@naver.com>
2023-05-01 01:17:56 +0900

    feat: RestaurantRepositoryImpl 구현
    RestaurantRepository 구현체

---
+++ src/main/java/com/software_engineering/booksys_ver2/restaurant/domain/repository/RestaurantRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.software_engineering.booksys_ver2.restaurant.domain.repository;
+
+import com.software_engineering.booksys_ver2.restaurant.domain.Restaurant;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+  private final EntityManager em;
+
+  @Override
+  public void save(Restaurant restaurant) {
+
+    em.persist(restaurant);
+  }
+
+  @Override
+  public Restaurant findById(Long id) {
+
+    return em.find(Restaurant.class, id);
+  }
+}
