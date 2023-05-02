62255f810e0d3f1e522d1df710d1941cc52cbb4d
jae woo Jung <awfjol2008@naver.com>
2023-05-02 09:32:40 +0900

    feat: 예약 취소 메소드 엔티티 레벨 구현

--- src/main/java/com/software_engineering/booksys_ver2/booking/domain/Booking.java
+++ src/main/java/com/software_engineering/booksys_ver2/booking/domain/Booking.java
@@ -4,12 +4,12 @@ import com.software_engineering.booksys_ver2.booking.domain.enums.BookingStatus;
 import com.software_engineering.booksys_ver2.customer.domain.Customer;
 import com.software_engineering.booksys_ver2.restaurant.domain.Restaurant;
 import com.software_engineering.booksys_ver2.table.domain.Table;
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
-import static jakarta.persistence.FetchType.LAZY;
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
@@ -53,12 +53,30 @@ public abstract class Booking {
   public abstract String getDetails();
 
   //== 연관관계 편의 메소드 ==//
+
   public void setCustomer(Customer customer) {
     this.customer = customer;
     customer.getBookings().add(this);
   }
+
   public void setRestaurant(Restaurant restaurant) {
     this.restaurant = restaurant;
     restaurant.getBookings().add(this);
+  }
+
+  //== 비즈니스 로직 ==//
+
+  /**
+   * 예약 취소
+   */
+  public void cancel() {
+    LocalDateTime currentDateTime = LocalDateTime.now();
+    LocalDateTime reservationDateTime = getBookingDateTime();
+
+    if (currentDateTime.isAfter(reservationDateTime.minusMinutes(30))) { // 예약시간 30분 전까지는 취소 가능, 예약 시간 임박 시에는 취소 불가
+      throw new IllegalStateException("예약 시간 임박으로 취소할 수 없습니다.");
+    }
+
+    this.setBookingStatus(BookingStatus.CANCEL); // 예약상태 취소로 변경
   }
 }
