4d6247358ce62a4d6f41fa4bb266cba6700c1914
jae woo Jung <awfjol2008@naver.com>
2023-05-02 09:29:26 +0900

    feat: Restaurant 엔티티 생성 메소드 구현

--- src/main/java/com/software_engineering/booksys_ver2/restaurant/domain/Restaurant.java
+++ src/main/java/com/software_engineering/booksys_ver2/restaurant/domain/Restaurant.java
@@ -1,8 +1,10 @@
 package com.software_engineering.booksys_ver2.restaurant.domain;
 
 import com.software_engineering.booksys_ver2.booking.domain.Booking;
-import jakarta.persistence.*;
+import javax.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.Vector;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Restaurant {
 
   @Id
@@ -22,5 +25,13 @@ public class Restaurant {
   @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
   private List<Booking> bookings = new Vector<>(); // 해당 레스토랑의 예약 목록
 
+  //== 생성 메소드 ==//
 
+  public static Restaurant createRestaurant(int tableQuantity) {
+
+    Restaurant restaurant = new Restaurant();
+    restaurant.setTableQuantity(tableQuantity);
+
+    return restaurant;
+  }
 }
