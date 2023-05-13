ce13538e04882e99ee30c1c5e7ce9315792d4085
jae woo Jung <awfjol2008@naver.com>
2023-05-13 17:49:55 +0900

    feat: 온라인 예약 request DTO 구현

---
+++ src/main/java/com/software_engineering/booksys_ver2/booking/presentation/dto/request/CreateReservationRequest.java
@@ -0,0 +1,25 @@
+package com.software_engineering.booksys_ver2.booking.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class CreateReservationRequest {
+
+  private String name;
+
+  private String phoneNumber;
+
+  private int tableNumber;
+
+  private int covers;
+
+  private String password;
+  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+  private LocalDateTime bookingDateTime;
+
+}
