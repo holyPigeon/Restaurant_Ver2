9c3483eb9132da67a91a2b69b8556eefcf857909
jae woo Jung <awfjol2008@naver.com>
2023-05-24 14:48:27 +0900

    feat: 온라인 예약 제약조건 로직 구현

--- src/main/java/com/software_engineering/booksys_ver2/booking/application/BookingService.java
+++ src/main/java/com/software_engineering/booksys_ver2/booking/application/BookingService.java
@@ -28,30 +28,56 @@ public class BookingService {
   private final TableRepository tableRepository;
 
   @Transactional
-  public Long createReservation(Long customerId, Long restaurantId, Long tableId, LocalDateTime bookingDateTime) {
+  public Long createReservation(Long customerId, Long restaurantId, Long tableId, LocalDateTime bookingDateTime, int covers) {
 
-    // 고객, 레스토랑, 테이블 조회
+    // 고객, 레스토랑, 테이블, 기존 예약 조회
     Customer customer = customerRepository.findById(customerId);
     Restaurant restaurant = restaurantRepository.findById(restaurantId);
     Table table = tableRepository.findById(tableId);
+    List<Booking> existingBookings = bookingRepository.findAll();
+
+    // 테이블 좌석 수보다 식사 인원이 더 많은 경우 예외 반환
+    if (covers > table.getPlaces()) {
+      throw new IllegalStateException("해당 테이블의 좌석 수가 식사 인원보다 더 많습니다.");
+    }
+
+    // 예약하려는 시간이 기존 예약 시간과 겹치는 경우 예외 반환
+    for (Booking existingBooking : existingBookings) {
+      if (isDoubleBooking(bookingDateTime, bookingDateTime.plusHours(2), existingBooking.getBookingDateTime(), existingBooking.getEndDateTime())) {
+        throw new IllegalArgumentException("해당 시간에 이미 예약이 존재합니다.");
+      }
+    }
 
     // 온라인 예약 생성 후 DB 저장
-    Booking reservation = Reservation.createReservation(customer, restaurant, table, bookingDateTime);
+    Booking reservation = Reservation.createReservation(customer, restaurant, table, bookingDateTime, covers);
     bookingRepository.save(reservation);
 
     return reservation.getId();
   }
 
   @Transactional
-  public Long createWalkIn(Long customerId, Long restaurantId, Long tableId) {
+  public Long createWalkIn(Long customerId, Long restaurantId, Long tableId, int covers) {
 
-    // 고객, 레스토랑, 테이블 조회
+    // 고객, 레스토랑, 테이블, 기존 예약 조회
     Customer customer = customerRepository.findById(customerId);
     Restaurant restaurant = restaurantRepository.findById(restaurantId);
     Table table = tableRepository.findById(tableId);
+    List<Booking> existingBookings = bookingRepository.findAll();
+
+    // 테이블 좌석 수보다 식사 인원이 더 많은 경우 예외 반환
+    if (covers > table.getPlaces()) {
+      throw new IllegalStateException("해당 테이블의 좌석 수가 식사 인원보다 더 많습니다.");
+    }
+
+    // 예약하려는 시간이 기존 예약 시간과 겹치는 경우 예외 반환
+    for (Booking existingBooking : existingBookings) {
+      if (isDoubleBooking(LocalDateTime.now(), LocalDateTime.now().plusHours(2), existingBooking.getBookingDateTime(), existingBooking.getEndDateTime())) {
+        throw new IllegalArgumentException("해당 시간에 이미 예약이 존재합니다.");
+      }
+    }
 
     // 현장 예약 생성 후 DB 저장
-    Booking walkIn = WalkIn.createWalkIn(customer, restaurant, table);
+    Booking walkIn = WalkIn.createWalkIn(customer, restaurant, table, covers);
     bookingRepository.save(walkIn);
 
     return walkIn.getId();
@@ -73,14 +99,19 @@ public class BookingService {
   }
 
   @Transactional
-  public void updateBooking(Long bookingId, int tableNumber, int customerCount, LocalDateTime bookingDateTime) {
+  public void updateBooking(Long bookingId, int tableNumber, int covers, LocalDateTime bookingDateTime) {
 
     // 에약, 테이블 조회
     Booking findBooking = bookingRepository.findById(bookingId);
     Table findTable = tableRepository.findByTableNumber(tableNumber);
 
+    // 테이블 좌석 수보다 식사 인원이 더 많은 경우 예외 반환
+    if (covers > findTable.getPlaces()) {
+      throw new IllegalStateException("해당 테이블의 좌석 수가 식사 인원보다 더 많습니다.");
+    }
+
     // 예약 시간 및 테이블 정보 변경
-    findTable.setCustomerCount(customerCount);
+    findBooking.setCovers(covers);
     findBooking.setBookingDateTime(bookingDateTime);
     findBooking.setTable(findTable);
   }
@@ -91,12 +122,8 @@ public class BookingService {
     bookingRepository.deleteById(bookingId);
   }
 
-  /*public void cancelBooking(Long bookingId) {
+  public boolean isDoubleBooking(LocalDateTime bookingStart, LocalDateTime bookingEnd, LocalDateTime existingBookingStart, LocalDateTime existingBookingEnd) {
 
-    // 예약 엔티티 조회
-    Booking booking = bookingRepository.findById(bookingId);
-
-    // 예약 취소
-    booking.cancel();
-  }*/
+    return bookingStart.isBefore(existingBookingEnd) && existingBookingStart.isBefore(bookingEnd);
+  }
 }
