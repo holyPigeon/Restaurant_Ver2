372a5f90a418a1a8ffdfc5b0fd7c07af052fbdf0
jae woo Jung <awfjol2008@naver.com>
2023-05-24 14:38:40 +0900

    feat: WalkIn 엔티티 변수명 변경

--- src/main/java/com/software_engineering/booksys_ver2/booking/domain/WalkIn.java
+++ src/main/java/com/software_engineering/booksys_ver2/booking/domain/WalkIn.java
@@ -4,16 +4,20 @@ import com.software_engineering.booksys_ver2.booking.domain.enums.BookingStatus;
 import com.software_engineering.booksys_ver2.customer.domain.Customer;
 import com.software_engineering.booksys_ver2.restaurant.domain.Restaurant;
 import com.software_engineering.booksys_ver2.table.domain.Table;
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "walkIns")
 @Getter @Setter
+@javax.persistence.Table(name = "walkIns")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Access(AccessType.FIELD)
 @DiscriminatorValue("WALKIN")
@@ -21,11 +25,11 @@ public class WalkIn extends Booking {
 
   public String getDetails() {
 
-    return "Walk-in (" + getTable().getCustomerCount() + "명)" ;
+    return "Walk-in (" + getTable().getPlaces() + "명)" ;
   }
 
   //==생성 메소드==//
-  public static WalkIn createWalkIn(Customer customer, Restaurant restaurant, Table table) {
+  public static WalkIn createWalkIn(Customer customer, Restaurant restaurant, Table table, int covers) {
 
     WalkIn walkIn = new WalkIn();
     walkIn.setBookingStatus(BookingStatus.BOOK);
@@ -34,6 +38,7 @@ public class WalkIn extends Booking {
     walkIn.setCustomer(customer);
     walkIn.setRestaurant(restaurant);
     walkIn.setTable(table);
+    walkIn.setCovers(covers);
 
     return walkIn;
   }
