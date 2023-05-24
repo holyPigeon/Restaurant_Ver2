beb2f1d56e1ec369a0abd705c35520c50563d830
jae woo Jung <awfjol2008@naver.com>
2023-05-24 14:54:06 +0900

    feat: CustomerController Swagger UI 관련 어노테이션 추가

--- src/main/java/com/software_engineering/booksys_ver2/customer/presentation/CustomerController.java
+++ src/main/java/com/software_engineering/booksys_ver2/customer/presentation/CustomerController.java
@@ -4,6 +4,8 @@ import com.software_engineering.booksys_ver2.customer.application.CustomerServic
 import com.software_engineering.booksys_ver2.customer.domain.Customer;
 import com.software_engineering.booksys_ver2.customer.presentation.dto.response.CustomerResponse;
 import com.software_engineering.booksys_ver2.customer.presentation.dto.response.CustomerListResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "고객 관련 API")
 public class CustomerController {
 
   private final CustomerService customerService;
@@ -21,6 +24,7 @@ public class CustomerController {
    * 전체 고객 조회
    */
   @GetMapping("/customer/list")
+  @ApiOperation(value = "전체 고객 조회 API", notes = "")
   public CustomerListResponse<List<CustomerResponse>> listCustomer() {
 
     List<Customer> customers = customerService.findAll();
