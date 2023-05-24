c9c574dfeb755ba27a43d77746e96b98b7d2dff5
jae woo Jung <awfjol2008@naver.com>
2023-05-24 14:40:04 +0900

    feat: Table 엔티티 변수명 변경

--- src/main/java/com/software_engineering/booksys_ver2/table/domain/Table.java
+++ src/main/java/com/software_engineering/booksys_ver2/table/domain/Table.java
@@ -15,22 +15,23 @@ import static javax.persistence.FetchType.LAZY;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Table {
 
-  @Id
-  @GeneratedValue
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="table_id")
   private Long id;
 
   private int tableNumber; // 테이블 번호
 
-  private int customerCount; // 식사 인원
+  private int places; // 테이블 좌석 수
 
   @OneToOne(fetch = LAZY)
+  @JoinColumn(name = "booking_id")
   private Booking booking; // 테이블에 할당된 예약
 
-  public static Table createTable(int tableNumber, int customerCount) {
+  public static Table createTable(int tableNumber, int places) {
 
     Table table = new Table();
     table.setTableNumber(tableNumber);
-    table.setCustomerCount(customerCount);
+    table.setPlaces(places);
 
     return table;
   }
